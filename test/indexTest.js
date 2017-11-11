6454d98a99d3fdc9d40831dfe2cae0841caa7077
STELLA PONG KEXIN <stellapong0513@hotmail.com>
2017-11-11 20:18:02 +0000

    automatically backed up by learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,16 +1,5 @@
 const expect = chai.expect;
 
-describe('index.js', function () {
-  describe('companyName', function () {
-    it('is set as Scuber', function () {
-      expect(companyName).to.equal('Scuber');
-    });
-
-    it('raises error if the companyName is changed', function () {
-      expect(function () { companyName = 'specialCompany' }).to.throw(TypeError);
-    });
-  });
-
   describe('mostProfitableNeighborhood', function () {
     it('is declared as equal to Chelsea', function () {
       expect(mostProfitableNeighborhood).to.equal('Chelsea');
